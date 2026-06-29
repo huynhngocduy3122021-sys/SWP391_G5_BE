e3b35589e691f39b4b55646714aa2aa8d004de9d
Bahashi <bachsy2005@gmail.com>
2026-06-29 19:54:32 +0700

    ko bt

--- src/main/java/Parking/dto/request/CreateParkingCardRequest.java
+++ src/main/java/Parking/dto/request/CreateParkingCardRequest.java
@@ -6,18 +6,15 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
 public class CreateParkingCardRequest {
     @NotBlank(message = "Card code is required")
     @Size(min = 3, max = 50, message = "Card code must be between 3 and 50 characters")
-    @Pattern(
-            regexp = "^[A-Za-z0-9\\-_]+$",
-            message = "Card code can only contain letters, numbers, '-' or '_'"
-    )
+    @Pattern(regexp = "^[A-Za-z0-9\\-_]+$", message = "Card code can only contain letters, numbers, '-' or '_'")
     private String cardCode;
 
-    
-     @NotNull(message = "ParkingBranchID cannot null")
+    @NotNull(message = "ParkingBranchID cannot null")
     private Long parkingBranchId;
 }
